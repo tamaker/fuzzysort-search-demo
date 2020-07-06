52e9ca0775e86ce169e824f183db443bf2232da8
AnthonyAmaker <tamaker@gmail.com>
2020-07-06 07:20:45 -0400

    initil commit

---
+++ script.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+    // THE ARRAY OUR SEARCHABLE CONTENT WILL BE PUSHED TO
+    //list = [];
+
+    // ITERATE THROUGH ALL THE P ELEMENTS AND PUSH THE JSON OBJECT
+    //WITH CONTENT AND IDX NUMBER TO THE ARRAY
+    $("#inner p").each(function (i, val) {
+          $(this).attr("id", "item-" + i);
+          newObj = { paraNum: i, paraText: $(val).text() };
+          list.push(newObj);
+    });
+
+/*
+    $(list).each(function(i, val){
+        newObj = { characterNum: i, characterText: $(val).characterName };
+        list.push(newObj);
+    })
+*/
+
+    console.log( list );
+
+
+    
+    return;
+    
+    
+
+    function doSearch(searchVal) {
+          console.log('doing search')
+          let objects = list;
+          let results = fuzzysort.go(searchVal, objects, {
+            keys: ['paraText'],
+            limit: 100, // don't return more results than you need!
+            allowTypo: false, // if you don't care about allowing typos
+            threshold: -10000, // don't return bad results,
+            separateWordSearch: false
+                
+          })
+
+          var bestResult = results[0];
+          console.log(results);
+          highlight(searchVal);
+          
+          /*
+          // When using multiple `keys`, results are different. They're indexable to get each normal result
+          fuzzysort.highlight(bestResult[0]) // 'Google <b>Chr</b>ome'
+          fuzzysort.highlight(bestResult[1]) // 'Launch <b>Chr</b>ome'
+          bestResult.obj.title // 'Google Chrome'
+          */
+    }
+    
+    
+    
+    function highlight(searchVal){
+          $('#inner p').each(function(i, val){
+                $('#inner p').eq(i).mark(searchVal);
+          })
+    }
+
+
+
+    // WAIT FOR INPUT TO STOP BEFORE RUNNING THE SEARCH
+    var timer = null;
+    $("#inputField").keydown(function () {
+          clearTimeout(timer);
+          timer = setTimeout(doStuff, 1000);
+    });
+
+    function doStuff() {
+          $("#inner p").unmark();
+          //$("span.highlight").removeClass("highlight");
+          //$('span.highlight').contents().unwrap();
+          //alert('do stuff');
+          doSearch($("#inputField").val());
+    }
+});
---
+++ test.php
@@ -0,0 +1,162 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mark.js/8.11.1/jquery.mark.min.js"></script>
+    <script src="https://rawgit.com/farzher/fuzzysort/master/fuzzysort.js"></script>
+    
+
+    <style>
+        #inner ul { list-style-type: none; }
+        .charImages { border: 1px solid block; border-radius: 50%; width: 80px; height: 80px; }
+        .item-text { 
+            min-height: 100px;
+            display: inline-flex;
+            align-items: center;
+            border: 1px solid aqua;
+        }
+    </style>
+
+    <script>
+        $(document).ready(function(){
+        
+            var list= [];
+            // THE ARRAY OUR SEARCHABLE CONTENT WILL BE PUSHED TO
+            $.getJSON( 'https://cdn.jsdelivr.net/gh/tamaker/fusejs-demo@master/got.js', function( data ) {
+                //console.log(data); //json output 
+
+                $(data[0].characters).each(function (i, val) {
+                    //console.log(val)
+                    //$(this).attr("id", "item-" + i);
+                    newObj = { itemNum: i, itemText: (val.houseName + ' - ' + val.characterName), itemPic: val.characterImageThumb  };
+                    list.push(newObj);
+
+                    
+                    if (val.characterImageThumb){
+                            $('#inner ul').append('<li class="item-' + i + '"><span class="item-text">' + val.characterName + '</span></li>');
+                            $('.item-' + i).prepend('<img class="charImages" src="' + val.characterImageThumb + '">')
+                    }
+                    
+                });
+
+                console.log(list);
+
+            });
+
+            // ITERATE THROUGH ALL THE P ELEMENTS AND PUSH THE JSON OBJECT
+            //WITH CONTENT AND IDX NUMBER TO THE ARRAY
+            
+
+
+            
+
+
+
+
+
+            function doSearch(searchVal) {
+                $('li').hide();
+                console.log('doing search');
+
+                
+                
+                let objects = list;
+                let results = fuzzysort.go(searchVal, list, {
+                    keys: ['itemText'],
+                    limit: 100, // don't return more results than you need!
+                    allowTypo: false, // if you don't care about allowing typos
+                    threshold: -10000, // don't return bad results,
+                    separateWordSearch: false
+                        
+                });
+
+                var bestResult = results[0];
+                console.log(results);
+                highlight(searchVal);
+                
+                /*
+                // When using multiple `keys`, results are different. They're indexable to get each normal result
+                fuzzysort.highlight(bestResult[0]) // 'Google <b>Chr</b>ome'
+                fuzzysort.highlight(bestResult[1]) // 'Launch <b>Chr</b>ome'
+                bestResult.obj.title // 'Google Chrome'
+                */
+
+
+                
+
+
+                
+                $(results).each(function(i, val){
+                    console.log('show item #' + val.obj.itemNum);
+                    setTimeout(function(){
+                        $('li.item-' + val.obj.itemNumm).show();
+                    }, 5000)
+                    
+                })
+
+            }
+
+
+
+            function highlight(searchVal){
+                $('span.item-text').each(function(i, val){
+                        $(this).mark(searchVal);
+                })
+            }
+
+
+
+            // WAIT FOR INPUT TO STOP BEFORE RUNNING THE SEARCH
+            var timer = null;
+            $("#inputField").keydown(function () {
+                clearTimeout(timer);
+                timer = setTimeout(doStuff, 1000);
+            });
+
+            function doStuff() {
+                $("span.item-text").unmark();
+                //$("span.highlight").removeClass("highlight");
+                //$('span.highlight').contents().unwrap();
+                //alert('do stuff');
+                doSearch($("#inputField").val());
+            }
+        })
+    </script>
+  </head>
+  <body>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+
+
+
+    <div id="wrapper">
+      <center>
+            <input type="text" id="inputField" style="width: 450px; margin: 40px; margin-bottom: 0px; padding: 20px; font-size: 1.4em; background-color: #EEE; border-radius: .4em; border: 2px solid #CCC;" placeholder="search for..." value=""><br>
+            <div class="statusText">
+                  Type a search word above, fuse.js is used to find and score matches found in each P element below.
+                  <pre></pre>
+            </div>
+      </center>
+      
+      
+      
+      
+      <div id="inner">
+                      
+            <ul>
+            </ul>
+      </div>
+</div>
+
+
+
+  </body>
+</html>
